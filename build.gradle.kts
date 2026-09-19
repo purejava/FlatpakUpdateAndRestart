b0077c50421bdbcf003ba379130ed782685afd90
Ralph Plawetzki <ralph@purejava.org>
2026-09-19 13:59:51 +0200

    Prepare release 1.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "org.purejava"
-version = "1.1.1"
+version = "1.1.2"
 description = "A Java library for updating Flatpak apps."
 
 val releaseGradlePluginToken: String = System.getenv("RELEASE_GRADLE_PLUGIN_TOKEN") ?: ""
