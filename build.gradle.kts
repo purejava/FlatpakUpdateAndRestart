f7aa717e8f29d99d2c896b71f1acb0936bfe74df
Ralph Plawetzki <ralph@purejava.org>
2026-09-19 14:05:51 +0200

    Increase artifact version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "org.purejava"
-version = "1.1.2"
+version = "1.1.3-SNAPSHOT"
 description = "A Java library for updating Flatpak apps."
 
 val releaseGradlePluginToken: String = System.getenv("RELEASE_GRADLE_PLUGIN_TOKEN") ?: ""
